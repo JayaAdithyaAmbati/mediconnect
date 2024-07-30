02e4eeda2618827597595dc9e525e2b09f2f2908
JayaAdithyaAmbati <a.jayaadithya@gmail.com>
2024-07-30 14:34:38 +0530

    Commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// require('dotenv').config();
+
+const config = {
+  awsAccessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  sesAccessKeyId: process.env.REACT_APP_SES_ACCESS_KEY_ID,
+  sesSecretAccessKey: process.env.REACT_APP_SES_SECRET_ACCESS_KEY,
+};
+
+export default config;
